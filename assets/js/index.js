f6787a9141a006a46c185812626ce1f2509bdcd7
Anwar Ismail <anwar.ismail@student.moringaschool.com>
2022-07-18 00:00:28 +0300

    Allow the user to input milk production andthe data stored

---
+++ assets/js/index.js
@@ -0,0 +1,10 @@
+let sum = 0;
+function getData() {
+  let form = document.querySelector("form");
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    addShedA(parseInt(e.target.sA.value));
+    addShedB(parseInt(e.target.sB.value));
+    addShedC(parseInt(e.target.sC.value));
+    addShedD(parseInt(e.target.sD.value));
+  })};
